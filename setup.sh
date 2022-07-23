c51b4b92c788790430f0afeb103a930efbf181d6
Aleksandr Maslyakov <adeacea@gmail.com>
2022-07-23 19:42:33 +0500

    Script for assets

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ echo Установка ассетов
 mkdir assets &&
 cd assets &&
 echo Скачиваю ассеты...
-curl https://i.ibb.co/vd66qHJ/dude.png --otuput dude.png
-curl https://i.ibb.co/VBLc6Bt/platform.png --otuput platform.png
-curl https://i.ibb.co/Vq5Vg74/sky.png --otuput sky.png
+curl https://i.ibb.co/vd66qHJ/dude.png -o dude.png
+curl https://i.ibb.co/VBLc6Bt/platform.png -o platform.png
+curl https://i.ibb.co/Vq5Vg74/sky.png -o sky.png
 
 
 
